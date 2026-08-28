39e308a121282a21156f36e063235476cd3018e1
Udit <uditrawat1905@gmail.com>
2026-08-28 23:58:10 +0530

    model: export the cache types from the package

--- glassbox/model/__init__.py
+++ glassbox/model/__init__.py
@@ -5,6 +5,7 @@ from glassbox.model.attention import (
     scaled_dot_product_attention,
 )
 from glassbox.model.blocks import TransformerBlock
+from glassbox.model.cache import KVCache, LayerCache
 from glassbox.model.config import GPTConfig
 from glassbox.model.feedforward import MLP, SwiGLU, build_feedforward
 from glassbox.model.gpt import GPT
@@ -14,6 +15,8 @@ from glassbox.model.rope import apply_rope, build_rope_cache, rotate_half
 __all__ = [
     "GPT",
     "GPTConfig",
+    "KVCache",
+    "LayerCache",
     "MLP",
     "MultiHeadAttention",
     "RMSNorm",
