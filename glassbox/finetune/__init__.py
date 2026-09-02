ba058204a9d3d3c1826fbc254c664f924e092771
Udit <uditrawat1905@gmail.com>
2026-09-02 23:25:13 +0530

    feature: export the finetune package surface

---
+++ glassbox/finetune/__init__.py
@@ -0,0 +1,21 @@
+from glassbox.finetune.lora import (
+    DEFAULT_TARGETS,
+    LoRALinear,
+    apply_lora,
+    load_adapter,
+    lora_parameters,
+    merge_lora,
+    save_adapter,
+    trainable_report,
+)
+
+__all__ = [
+    "DEFAULT_TARGETS",
+    "LoRALinear",
+    "apply_lora",
+    "load_adapter",
+    "lora_parameters",
+    "merge_lora",
+    "save_adapter",
+    "trainable_report",
+]
